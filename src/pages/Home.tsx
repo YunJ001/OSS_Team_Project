e4e91180de552e3698cdaeb69f8a77504a280143
Wookangsik <wks09254930@gmail.com>
2025-05-25 23:36:12 +0900

    Basic setup of the Home section & creating cards for each section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import backgroundImage from '../assets/main_background.jpg';
+import { FaMusic, FaVolumeMute } from 'react-icons/fa';
+
 const Home = () => {
+  const navigate = useNavigate();
+  const [soundOn, setSoundOn] = useState(true);
+
+  const cards = [
+    {
+      title: "Today's Word",
+      description: '오늘의 영단어를 확인해보세요.',
+      route: '/word',
+    },
+    {
+      title: 'Test',
+      description: '배운 단어를 테스트해요!\n(오늘의 단어 확인 후 보기 가능.)',
+      route: '/test',
+    },
+    {
+      title: 'Review',
+      description: '공부한 내용을 복습해요!\n(테스트 후 보기 가능.)',
+      route: '/review',
+    },
+  ];
+
   return <div>Home</div>;
 };
 
