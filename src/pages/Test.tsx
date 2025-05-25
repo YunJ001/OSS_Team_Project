6b59d3fa6c93b61218672922ad01b81e0d272a9f
Wookangsik <wks09254930@gmail.com>
2025-05-25 23:48:02 +0900

    Test: Basic Setup

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,4 +1,17 @@
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import backgroundImage from '../assets/background_2.jpg';
+
+interface WordData {
+  english: string;
+  korean: string;
+}
+
 const Test = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const allWords: WordData[] = location.state?.words || [];
+
   return <div>Test</div>;
 };
 
