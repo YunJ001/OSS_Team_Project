5d05f992fcbca258e3f824809d2276df3530894b
Wookangsik <wks09254930@gmail.com>
2025-05-25 23:58:12 +0900

    Test: Divide Sections & Declare Basic Variables

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,17 +2,40 @@ import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import backgroundImage from '../assets/background_2.jpg';
 
+// 단어 데이터 타입 정의의
 interface WordData {
   english: string;
   korean: string;
 }
 
 const Test = () => {
+  // 페이지 이동 횟수
   const navigate = useNavigate();
+  // 이전 페이지에서 넘겨준 state 접근
   const location = useLocation();
+  // word 섹션에서 넘겨준 단어 배열을 받아옴.
   const allWords: WordData[] = location.state?.words || [];
 
-  return <div>Test</div>;
+  const [currentIndex, setCurrentIndex] = useState(-1); // 테스트 시작 전
+  const [userAnswer, setUserAnswer] = useState(''); // 사용자의 입력값
+  const [score, setScore] = useState(0); // 맞힌 갯수
+  const [isFinished, setIsFinished] = useState(false); // 테스트 종료 여부
+
+  if (allWords.length === 0) {
+    return <div className="bg-black text-white h-screen flex justify-center items-center">단어 데이터가 없습니다. 홈으로 돌아가주세요.</div>;
+  }
+
+  return (
+    <div>
+      {/* 테스트 시작 전 화면 */}
+      {currentIndex === -1 ? (
+      ): isFinished ? (
+      // 테스트 완료 화면
+      ) : (
+      // 테스트 문제 풀이 화면
+      )}
+    </div>
+  );
 };
 
 export default Test;
